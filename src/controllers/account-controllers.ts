cbbb34e5e4e42cfe66d7643dec5fe4aa71c5167d
mihajatiana <hei.mihajatiana@gmail.com>
2026-03-08 16:15:07 +0300

    feat(auth): add POST /auth/sign-in/google route and controller

--- src/controllers/account-controllers.ts
+++ src/controllers/account-controllers.ts
@@ -1,6 +1,7 @@
 import { RequestHandler } from "express";
 import { v4 } from "uuid";
 
+import { ApiError } from "@/errors";
 import { AccountServices } from "@/services";
 import { errorWrapper } from "@/utilities";
 import { AccountValidator } from "@/validator";
@@ -22,6 +23,16 @@ export class AccountController {
       AccountValidator.signUp(account);
       const createdUser = await AccountServices.singUp(v4(), account);
       res.json(createdUser);
+    } catch (err) {
+      next(err);
+    }
+  };
+  static readonly signInWithGoogle: RequestHandler = async (req, res, next) => {
+    try {
+      const { googleId, email, name } = req.body;
+      if (!googleId || !email) throw new ApiError("googleId and email are required", 400);
+      const data = await AccountServices.signInWithGoogle(googleId, email, name);
+      res.json(data);
     } catch (err) {
       next(err);
     }
